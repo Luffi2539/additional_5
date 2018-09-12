a3b14a26de9313010baa19b91ff44c8253aa45a1
Luffi2539 <vkkovalev91@gmail.com>
2018-09-13 02:38:40 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    var strArr=str.split('');
+    var bc=bracketsConfig;
+    for(var i=0;i<strArr.length;i++){
+      
+      for(var j=0;j<bc.length;j++){
+        if(strArr[i]===bc[j][0] && strArr[i+1]===bc[j][1]){
+          strArr.splice(i,2);
+          if (strArr.length==0){
+
+              return true;
+          }
+          i=-1;
+        }
+      }
+    }
+    return false;
+  }
